8dfbdb326eef2a049740ef2b0e77de193c9a300a
Alexandre Macquin <alexandre.macquin@gmail.com>
2020-08-13 15:10:14 +0200

    done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+  # GET /cocktails
+  before_action :set_cocktail, only: [ :show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # GET /cocktails/1
+  def show
+  end
+
+  # GET /cocktails/new
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  # POST /cocktails
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktails_path, notice: 'cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+      # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+      # Only allow a trusted parameter "white list" through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+end
